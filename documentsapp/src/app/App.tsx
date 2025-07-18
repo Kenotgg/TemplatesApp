99dd467d1a773b4acd79dd5f8a50ecc5649c60fd
Kenotgg <130081795+Kenotgg@users.noreply.github.com>
2025-07-19 00:15:29 +0500

    Немного улучшено оформление

--- documentsapp/src/app/App.tsx
+++ documentsapp/src/app/App.tsx
@@ -3,19 +3,26 @@ import { Routes, Route, Link } from 'react-router-dom';
 import TemplatesPage from '@/pages/templatesList/ui/templatesPage';
 import TemplateDetailsPage from '@/entities/template/ui/TemplateCard/templateDetailsPage';
 import PageNotFoundPage from '@/shared/ui/pageNotFound/pageNotFoundPage';
+import { Box } from '@chakra-ui/react';
 export default function App() {
     return (
-        <>
-        <header>
-             <Link to='/all'>Все шаблоны</Link>
-             <Link to='*'>Профиль</Link>
-        </header>
-        <Routes>
-            <Route path='/all' element={<TemplatesPage/>}/>
-            <Route path='/template/:id' element={<TemplateDetailsPage/>}/>
-            <Route path='*' element={<PageNotFoundPage/>}/>
-        </Routes>
-        </>
+        <Box minHeight={500}>
+            <Box width={'full'}>
+                <Link style={{marginRight:5}} to='/templates'>Главная</Link>
+                <Link style={{marginRight:5}} to='/profile'>Профиль</Link>
+                <Link style={{marginRight:5}} to='/profile'>Войти</Link>
+            </Box>
+            <Routes>
+                <Route path='/templates' element={<TemplatesPage />} />
+                <Route path='/template/:id' element={<TemplateDetailsPage />} />
+                <Route path='/login' element={<PageNotFoundPage />} />
+                <Route path='/profile' element={<PageNotFoundPage />} />
+
+                <Route path='*' element={<PageNotFoundPage />} />
+            </Routes>
+        </Box>
+
+
     )
 }
 
